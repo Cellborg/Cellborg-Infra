1617a4ccf4d9f9891c0d2c93f2874950b72b01a7
Seshagiri Nuthalapati <nasgrao@gmail.com>
2025-02-18 22:28:52 -0800

    added price.

--- terraform-iac/bedrock/bedrock.tf
+++ terraform-iac/bedrock/bedrock.tf
@@ -185,7 +185,8 @@ resource "aws_instance" "nat" {
     market_type = "spot"
     spot_options {
       instance_interruption_behavior = "stop"
-      spot_instance_type                   = "persistent"
+      spot_instance_type             = "persistent"
+      max_price                      = 0.003
     }
   }
 
