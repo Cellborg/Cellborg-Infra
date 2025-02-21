362d49fd2cd33dccb01c65af2e9507e357e937f0
Seshagiri Nuthalapati <nasgrao@gmail.com>
2025-02-20 22:38:45 -0800

    create reqd ecs roles

--- terraform-iac/bedrock/bedrock.tf
+++ terraform-iac/bedrock/bedrock.tf
@@ -270,3 +270,67 @@ resource "aws_security_group" "private" {
 resource "aws_ecs_cluster" "cellborg_ecs_cluster" {
   name = "cellborg-ecs-cluster"
 }
+
+# create role
+# ECS Execution Role
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+# API Task Role
+resource "aws_iam_role" "api_task_role" {
+  name = "Cellborg-ApiTaskRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
+
+# Frontend Task Role
+resource "aws_iam_role" "frontend_task_role" {
+  name = "Cellborg-FrontendTaskRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+}
