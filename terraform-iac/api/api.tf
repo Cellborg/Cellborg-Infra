1f6208fa8220cc6722fd64c1f3df70149964642b
Seshagiri Nuthalapati <nasgrao@gmail.com>
2025-02-23 15:25:44 -0800

    config change

--- terraform-iac/api/api.tf
+++ terraform-iac/api/api.tf
@@ -84,9 +84,12 @@ resource "aws_ecs_capacity_provider" "api_ecs_spot_capacity_provider" {
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs_spot_asg.arn
     managed_termination_protection = "ENABLED"
-    target_capacity           = 100
-    minimum_scaling_step_size = 1
-    maximum_scaling_step_size = 2
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 100
+    }
   }
 }
 
